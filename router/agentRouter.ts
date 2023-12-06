d4e252d85421738ef599ae8e86737e12a24779e8
Godswill Udida <udidagodswill7@gmail.com>
2023-12-06 12:56:53 +0100

    udida

--- router/agentRouter.ts
+++ router/agentRouter.ts
@@ -6,8 +6,8 @@ const router = Router();
 router.route("/create-agent").post(registerAgent);
 router.route("/sign-in-agent").post(signInAgent);
 router.route("/:token/verify-agent").patch(verifyAgent);
-router.route("/delete-agent").delete(deleteAgent);
+router.route("/:agentID/delete-agent").delete(deleteAgent);
 router.route("/view-all-agents").get(viewAllAgent);
-router.route("/:userID/view-one-agent").get(viewOneAgent);
+router.route("/:agentID/view-one-agent").get(viewOneAgent);
 
 export default router;
